3546ddb9983a7a090adaf29bb59f55d1097c93c9
Stefan Sundin <stefan@stefansundin.com>
2015-01-18 12:40:07 -0800

    Update snippets.rb

--- snippets.rb
+++ snippets.rb
@@ -20,6 +20,14 @@ rake db:schema:dump db:structure:dump
 rake assets:precompile
 redis-cli FLUSHALL
 
+# rbenv
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update
+# add to .bash_profile
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
 # Gems
 group :development do
   # debug errors on exception
