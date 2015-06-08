594749275635fdf6cd46aeebf5062994ab3c7022
Stefan Sundin <stefan@stefansundin.com>
2015-06-07 20:41:03 -0700

    Update snippets.c

--- snippets.c
+++ snippets.c
@@ -1,3 +1,8 @@
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
+
+# get filesize
+FILE *f = fopen("file", "rb");
+fseek(f, 0, SEEK_END);
+int filesize = ftell(f);
