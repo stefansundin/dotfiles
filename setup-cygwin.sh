8b26b8d7c501d9cf6b340b42cbe629419f16c9f1
Stefan Sundin <stefan@stefansundin.com>
2015-07-07 23:26:45 -0700

    Update setup-cygwin.sh

--- setup-cygwin.sh
+++ setup-cygwin.sh
@@ -1,3 +1,3 @@
-git vim libreadline-devel mingw64-i686-gcc-core mingw64-x86_64-gcc-core patch php-curl php-simplexml php-bcmath
+git vim libreadline-devel mingw64-i686-gcc-core mingw64-x86_64-gcc-core patch php-curl php-simplexml php-bcmath libpq-devel
 
 alias ls='ls -p'
