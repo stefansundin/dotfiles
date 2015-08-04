44253bf7480d3ef71d6d6caa430595819d57bba5
Stefan Sundin <stefan@stefansundin.com>
2015-08-04 14:32:49 -0700

    Update snippets.sql

--- snippets.sql
+++ snippets.sql
@@ -1,2 +1,5 @@
+-- get number of active database connections in postgres
+SELECT COUNT(*) FROM pg_stat_activity;
+
 -- get count of column values
 SELECT locale, COUNT(locale) AS locale_occurrence FROM users GROUP BY locale ORDER BY locale_occurrence DESC;
