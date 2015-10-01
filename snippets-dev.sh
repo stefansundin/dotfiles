bddb485213e856a7946770209b8e8acd813a82da
Stefan Sundin <stefan@stefansundin.com>
2015-09-30 21:40:38 -0700

    Update snippets-dev.sh

--- snippets-dev.sh
+++ snippets-dev.sh
@@ -1,1 +1,4 @@
 echo "data:image/png;base64,`cat image.png | base64`" | pbcopy
+
+# Subresource integrity hashing https://srihash.org/
+echo -n "sha384-"; curl -s https://code.jquery.com/jquery-2.1.4.min.js | openssl dgst -sha384 -binary | openssl enc -base64 -A
