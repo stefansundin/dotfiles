08e1f946c7043cbf43d93c16c62c6e566ee9fa0c
Stefan Sundin <git@stefansundin.com>
2015-10-18 23:04:09 -0700

    Update snippets-dev.sh

--- snippets-dev.sh
+++ snippets-dev.sh
@@ -2,3 +2,7 @@ echo "data:image/png;base64,`cat image.png | base64`" | pbcopy
 
 # Subresource integrity hashing https://srihash.org/
 echo -n "sha384-"; curl -s https://code.jquery.com/jquery-2.1.4.min.js | openssl dgst -sha384 -binary | openssl enc -base64 -A ;echo
+
+# Generate SSL key and CSR
+openssl genrsa -out domain.com.key 2048
+openssl req -new -sha256 -key domain.com.key -out domain.com.csr
