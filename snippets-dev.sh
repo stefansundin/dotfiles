dc0a2a9c3aad2b4463439099ad31e3c00619bfb4
Stefan Sundin <git@stefansundin.com>
2016-06-09 22:18:20 -0700

    Update snippets-dev.sh

--- snippets-dev.sh
+++ snippets-dev.sh
@@ -3,10 +3,20 @@ echo "data:image/png;base64,`cat image.png | base64`" | pbcopy
 # Subresource integrity hashing https://srihash.org/
 echo -n "sha384-"; curl -s https://code.jquery.com/jquery-2.1.4.min.js | openssl dgst -sha384 -binary | openssl enc -base64 -A ;echo
 
-# Generate SSL key and CSR
-openssl genrsa -out domain.com.key 2048
+# S/MIME certificate:
+# Validate your email with certificate authority.
+# Generate key and csr:
+openssl req -newkey rsa:4096 -keyout your-email.key -out your-email.csr
+# Submit csr to certificate authority. Download crt.
+# Combine crt and key to pfx:
+openssl pkcs12 -export -in your-email.crt -inkey your-email.key -out your-email.pfx
+# Import pfx into browser and email client.
+
+# Web server certificate:
+# Generate key and csr:
+openssl genrsa -out domain.com.key 4096
 openssl req -new -sha256 -key domain.com.key -out domain.com.csr
-# After the CSR is approved, save the cert to domain.com.crt
+# After the csr is approved, save the cert to domain.com.crt
 wget https://www.startssl.com/certs/sub.class1.server.ca.pem
 cat domain.com.crt sub.class1.server.ca.pem > domain.com-unified.crt
 # Print cert information
