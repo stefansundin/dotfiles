eaa221cc9b23e18031bd76fc70eb751c2475f1d2
Stefan Sundin <git@stefansundin.com>
2016-07-28 16:44:35 -0700

    Update snippets.sql

--- snippets.sql
+++ snippets.sql
@@ -3,3 +3,10 @@ SELECT COUNT(*) FROM pg_stat_activity;
 
 -- get count of column values
 SELECT locale, COUNT(locale) AS locale_occurrence FROM users GROUP BY locale ORDER BY locale_occurrence DESC;
+
+-- query for locks
+SELECT t.relname,l.locktype,page,virtualtransaction,pid,mode,granted FROM pg_locks l, pg_stat_all_tables t WHERE l.relation=t.relid ORDER BY relation ASC;
+
+-- kill a lock
+SELECT pg_cancel_backend(pid);
+
