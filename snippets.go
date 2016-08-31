da39002713ca901b4475e0ee1e721d809f5c08f1
Stefan Sundin <git@stefansundin.com>
2016-08-31 15:10:39 -0700

    Update snippets.go

--- snippets.go
+++ snippets.go
@@ -2,3 +2,9 @@
 mkdir -p vendor/github.com/aws
 cd vendor/github.com/aws
 git submodule add https://github.com/aws/aws-sdk-go
+
+# compile smaller binaries - https://golang.org/cmd/link/
+go build -ldflags="-s -w"
+
+# cross-compile - https://golang.org/doc/install/source#environment
+GOOS=linux GOARCH=amd64 go build
