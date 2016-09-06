338722e3db69c0321a2a126e1b984a997becd7d7
Stefan Sundin <git@stefansundin.com>
2016-09-06 13:37:13 -0700

    Update snippets-git.sh

--- snippets-git.sh
+++ snippets-git.sh
@@ -17,6 +17,7 @@ git config --global alias.b 'checkout -b'
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.cim 'commit --allow-empty-message -m ""'
+git config --global alias.cl 'clone --recursive -j8'
 git config --global alias.co checkout
 git config --global alias.con 'branch -a --contains'
 git config --global alias.d '!git diff HEAD~${1:-1} HEAD~$((${1:-1}-1)) ${*:2} #'
