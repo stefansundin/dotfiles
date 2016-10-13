5dad4d9644260395447c82956e4a7839e4c89c8d
Stefan Sundin <git@stefansundin.com>
2016-10-13 13:08:54 -0700

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -2,3 +2,13 @@
 var d = new Date();
 var dst = d.getTimezoneOffset() < Math.max(new Date(d.getFullYear(),0,1).getTimezoneOffset(),new Date(d.getFullYear(),6,1).getTimezoneOffset());
 window.location.replace(window.location+'&timezone='+d.getTimezoneOffset()+(dst?"&dst":""));
+
+// convert query string to object
+function toObject(arr) {
+  var obj = {};
+  arr.forEach(function(e) {
+    obj[e[0]] = e[1];
+  });
+  return obj;
+}
+var args = toObject(window.location.search.substr(1).split("&").map(function(arg){ return arg.split("="); }));
