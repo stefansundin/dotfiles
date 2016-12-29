ccff027530ca2fecb4c5ed9205f487c6b99ef6f6
Stefan Sundin <git@stefansundin.com>
2016-12-29 00:18:11 -0800

    Update setup-pi.sh

--- setup-pi.sh
+++ setup-pi.sh
@@ -1,6 +1,12 @@
 # http://pinout.xyz/
 sudo raspi-config
 
+# 7 inch display GPIO: https://www.modmypi.com/image/data/tutorials/raspberry-pi-7-touch-screen-assembly-guide/16.jpg
+# Red    – 5V  - Pin 2
+# Black  – GND - Pin 6
+# Green  – SDA - Pin 3
+# Yellow – SCL - Pin 5
+
 # rotate display output
 sudo vim /boot/config.txt
 lcd_rotate=2
