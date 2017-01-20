9e89275088a1f5ba430e05ad71d1e791de30b5fd
Stefan Sundin <git@stefansundin.com>
2017-01-19 20:45:19 -0800

    Update snippets.sql

--- snippets.sql
+++ snippets.sql
@@ -10,3 +10,6 @@ SELECT t.relname,l.locktype,page,virtualtransaction,pid,mode,granted FROM pg_loc
 -- kill a lock
 SELECT pg_cancel_backend(pid);
 
+-- remove mysql password
+--mysqladmin -u root -p CURRENT_PASSWORD password ""
+SET PASSWORD FOR root@localhost=PASSWORD('');
