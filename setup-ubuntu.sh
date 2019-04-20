bc5ee0d00516e68fc704a957b924bf03168a6b88
Stefan Sundin <git@stefansundin.com>
2019-04-20 15:58:44 -0700

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -120,6 +120,16 @@ wget -O- https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key 
 echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
 sudo apt-get update
 sudo apt-get install resilio-sync
+# post install - https://help.resilio.com/hc/en-us/articles/206178924-Installing-Sync-package-on-Linux
+# WebUI: http://localhost:8888/gui/
+# user install:
+sudo systemctl stop resilio-sync.service
+sudo systemctl disable resilio-sync.service
+sudo sed -i 's/WantedBy=multi-user.target/WantedBy=default.target/g' /usr/lib/systemd/user/resilio-sync.service
+sudo systemctl daemon-reload
+sudo systemctl --user enable resilio-sync
+sudo systemctl --user start resilio-sync
+# system install:
 sudo usermod -a -G rslsync stefan
 
 # MKVToolNix - https://mkvtoolnix.download/downloads.html#ubuntu
