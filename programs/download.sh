0512935a47c4527e019c164fa82154dc2dc6d005
Stefan Sundin <git@stefansundin.com>
2019-09-24 22:11:51 -0700

    Create download.sh

---
+++ programs/download.sh
@@ -0,0 +1,5 @@
+# https://github.com/ytdl-org/youtube-dl/blob/master/README.md#options
+youtube-dl [...]
+
+# https://streamlink.github.io/cli.html
+streamlink --hls-live-restart -o video.mp4 [...] best
