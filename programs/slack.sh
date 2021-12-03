3f6f1e9a29bc9e723fba37d940c187fecf6a61f0
Stefan Sundin <git@stefansundin.com>
2021-12-03 15:42:37 -0800

    Create slack.sh

---
+++ programs/slack.sh
@@ -0,0 +1,4 @@
+# Open the developer tools in the Slack desktop app (tested and working on version 4.22.0):
+export SLACK_DEVELOPER_MENU=true
+/Applications/Slack.app/Contents/MacOS/Slack
+# Then press Ctrl+Alt+I / Cmd+Option+I
